edc642aed2bdbf81703c98ca0f7234ea23586786
Allan Anderson <allana@shaw.ca>
2016-09-26 17:04:25 -0600

    Added first POCO class

---
+++ ChinookSample/ChinookSystem.Data/POCOs/CustomerContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is a plain class so no need to add addtional Namespaces
+
+namespace ChinookSystem.Data.POCOs
+{
+    public class CustomerContact
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
