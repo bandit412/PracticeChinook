f7c42f8c1de504426aecb53206f1bf2649e9e720
Allan Anderson <allana@shaw.ca>
2016-09-27 10:28:44 -0600

    Added POCOs

---
+++ ChinookSample/ChinookSystem.Data/POCOs/TrackInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Data.POCOs
+{
+    public class TrackInfo
+    {
+        public string Title { get; set; }
+        public int NumberOfTracks { get; set; }
+        public decimal TotalTrackPrice { get; set; }
+        public double AveragetrackLengthInSeconds { get; set; }
+    }
+}
