5b0fa153f62a4980266fa44ed1358a60f7061ac5
Allan Anderson <allana@shaw.ca>
2016-10-13 16:40:56 -0600

    Add additional tracking properties for ApplicationUser
    Fields can can act as foreign key values into the user data

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -10,7 +10,12 @@ using Microsoft.AspNet.Identity.EntityFramework; // for IdentityUser
 
 namespace ChinookSystem.Security
 {
+    // This class represents logged on users
+    // You can add User data for the user by adding more properties to you User class
     public class ApplicationUser : IdentityUser
     {
+        // Can be either an Employee or Customer, thus the nullable int data types
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
     }
 }
