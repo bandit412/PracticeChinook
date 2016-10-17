3dd5b8d77f5f93195b51f7c8b4dfa9647fe17f17
Allan Anderson <allana@shaw.ca>
2016-10-17 17:06:55 -0600

    Activated basic security on a web page

--- ChinookSample/WebSite/Contact.aspx
+++ ChinookSample/WebSite/Contact.aspx
@@ -14,4 +14,8 @@
         <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
         <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
     </address>
+
+    <h2>Remember to to remove these lines before production!</h2>
+    <p>This is the login for the Web Master: userid = Webmaster&nbsp;&nbsp;&nbsp;password = Pa$$word1</p>
+    
 </asp:Content>
--- ChinookSample/WebSite/Queries/RepresentativeCustomers.aspx.cs
+++ ChinookSample/WebSite/Queries/RepresentativeCustomers.aspx.cs
@@ -9,6 +9,22 @@ public partial class Queries_RepresentativeCustomers : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!Page.IsPostBack)
+        {
+            // Are you logged in?
+            if (!Request.IsAuthenticated)
+            {
+                Response.Redirect("~/Account/Login.aspx");
+            }
+            // These lines are commented out as an authenticated user can access this page
+            //else
+            //{
+            //    // Are you allowed to be on this page?
+            //    if (!User.IsInRole(SecurityRoles.WebsiteAdmins))
+            //    {
+            //        Response.Redirect("~/Default.aspx");
+            //    }
+            //}
+        }
     }
 }
