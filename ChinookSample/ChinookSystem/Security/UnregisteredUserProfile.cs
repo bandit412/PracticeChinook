2d493645828009f531580630fc6244201322594e
Allan Anderson <allana@shaw.ca>
2016-10-18 16:08:31 -0600

    Setup of profile memberships
    Used to carry data to controls

---
+++ ChinookSample/ChinookSystem/Security/UnregisteredUserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnregisteredUserType
+    {
+        Undefined, Employee, Customer
+    }
+
+    public class UnregisteredUserProfile
+    {
+        public string UserId { get; set; } // generated when a user is added
+        public string UserName { get; set; } // collected
+        public string Email { get; set; } // collected
+        public string FirstName { get; set; } // comes from User table
+        public string LastName { get; set; } // comes from User table
+        public UnregisteredUserType UserType { get; set; }
+    }
+}
